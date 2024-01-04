67240768008500010145e7d97f37ef233f310b09
herbertnikolajewskidci <herbert.nikolajewski@digitalcareerinstitute.org>
2024-01-04 14:53:23 +0100

    inital commit

---
+++ astro.config.mjs
@@ -0,0 +1,37 @@
+import tailwind from "@astrojs/tailwind";
+import storyblok from "@storyblok/astro";
+import basicSsl from "@vitejs/plugin-basic-ssl";
+import { defineConfig } from "astro/config";
+import { loadEnv } from "vite";
+const env = loadEnv("", process.cwd(), "STORYBLOK");
+
+// https://astro.build/config
+export default defineConfig({
+    integrations: [
+        storyblok({
+            //accessToken: env.STORYBLOK_TOKEN,
+            accessToken: "hWdryci5Qm0EPyzhCfFQRwtt",
+            apiOptions: {
+                region: "",
+            },
+            bridge: {
+                customParent: "https://app.storyblok.com",
+            },
+            components: {
+                page: "storyblok/Page",
+                feature: "storyblok/Feature",
+                grid: "storyblok/Grid",
+                teaser: "storyblok/Teaser",
+                picslider: "storyblok/PicSlider",
+                slide: "storyblok/Slide",
+            },
+        }),
+        tailwind(),
+    ],
+    vite: {
+        plugins: [basicSsl()],
+        server: {
+            https: true,
+        },
+    },
+});
